6317f7a0085e7954b4f56cfa115adbf84ca8d093
rvl224 <vimyc95@gmail.com>
2023-04-06 23:27:27 +0800

    update

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -4,6 +4,3 @@ docker run -it --rm --privileged --network=host \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     --env="QT_X11_NO_MITSHM=1" \
     -e DISPLAY=$DISPLAY bytetrack-openvino /bin/bash
-    
-
-# docker run -it -v $PWD:/app/ --device=/dev/video0:/dev/video0 -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY
