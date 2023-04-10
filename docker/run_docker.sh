35f010de1ece40304740dbfad1f11b66d53734ae
rvl224 <vimyc95@gmail.com>
2023-04-10 16:25:55 +0800

    fix

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,6 +1,6 @@
 xhost +local:root
 docker run -it --rm --privileged --network=host \
-    -v /dev/video*:/dev/video* \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     --env="QT_X11_NO_MITSHM=1" \
     -e DISPLAY=$DISPLAY bytetrack-openvino /bin/bash
+
